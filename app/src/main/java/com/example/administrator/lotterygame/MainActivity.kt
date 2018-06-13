817e79e810499c2442ecd8e0f0c247ce371acbea
Marcus Lundy <mlundy360@gmail.com>
2018-06-13 16:44:58 -0400

    Updates

--- app/src/main/java/com/example/administrator/lotterygame/MainActivity.kt
+++ app/src/main/java/com/example/administrator/lotterygame/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.administrator.lotterygame
 import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.Bitmap
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -50,11 +55,28 @@ class MainActivity : AppCompatActivity() {
                 4 -> pic3.setImageResource(R.drawable.game7)
             }
 
-            if ((R.id.pic1) == (R.id.pic2) && (R.id.pic2) == (R.id.pic3) && (R.id.pic1) == (R.id.pic3)) {
+            /*Log.e("Pic2 is showing", pic2.getDrawable().toString())
+            Log.e("Pic1 is showing", pic1.getDrawable().toString())
+            Log.e("Pic3 is showing", pic3.getDrawable().toString())
+            Log.e("Is pic2 = pic1", pic1.getDrawable().toString().equals(pic2.getDrawable().toString()).toString())
+
+            val bitmap = (pic1.getDrawable() as BitmapDrawable).bitmap
+            val bitmap2 = (pic2.getDrawable() as BitmapDrawable).bitmap
+
+            if (bitmap == bitmap2) {
+                Log.e("Pic2 is showing", pic2.getDrawable().toString())
+            }*/
+
+            if (pic1.getDrawable().toString() == pic2.getDrawable().toString() &&
+                    pic2.getDrawable().toString() == pic3.getDrawable().toString() &&
+                    pic1.getDrawable().toString() == pic3.getDrawable().toString()) {
+
                 lblMessage.text = ("Congrats! You have matched all three numbers and gained 20 coins!")
                 counter += 20
                 lblCoinCounter.text = ("Coins = " + counter)
-            } else if ((R.id.pic1) == (R.id.pic2) || (R.id.pic2) == (R.id.pic3) || (R.id.pic1) == (R.id.pic3)) {
+            } else if (pic1.getDrawable().toString() == pic2.getDrawable().toString() ||
+                    pic2.getDrawable().toString() == pic3.getDrawable().toString() ||
+                    pic1.getDrawable().toString() == pic3.getDrawable().toString()) {
                 lblMessage.text = ("Congrats! You have matched two numbers and gained 10 coins!")
                 counter += 10
                 lblCoinCounter.text = ("Coins = " + counter)
