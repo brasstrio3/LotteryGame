fe9c732feb60421ca75938abf3214450e6f06576
Marcus Lundy <mlundy360@gmail.com>
2018-06-14 16:51:12 -0400

    Updates pt2

--- app/src/main/java/com/example/administrator/lotterygame/Main2Activity.kt
+++ app/src/main/java/com/example/administrator/lotterygame/Main2Activity.kt
@@ -2,11 +2,108 @@ package com.example.administrator.lotterygame
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main2.*
+import java.util.*
+
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        spin()
+    }
+
+    //Generates a random number between 1 and 4. Will be used to display pictures later
+    private fun generateRandomNumber(): Int {
+        val number = Random()
+        val min = 1
+        val max = 4
+        val num = number.nextInt(max + 1 - min) + min
+        return num
+    }
+
+    private fun spin() {
+        //TODO: this is the issue
+        var counter = txtUserCoins
+
+        var firstPic: Int = 0
+        var secondPic: Int = 0
+        var thirdPic: Int = 0
+
+        btnSpin.setOnClickListener() {
+            when (generateRandomNumber()) {
+                1 -> {
+                    pic1.setImageResource(R.drawable.bell)
+                    firstPic = 1
+                }
+                2 -> {
+                    pic1.setImageResource(R.drawable.cherries)
+                    firstPic = 2
+                }
+                3 -> {
+                    pic1.setImageResource(R.drawable.lemon)
+                    firstPic = 3
+                }
+                4 -> {
+                    pic1.setImageResource(R.drawable.game7)
+                    firstPic = 4
+                }
+            }
+            when (generateRandomNumber()) {
+                1 -> {
+                    pic2.setImageResource(R.drawable.bell)
+                    secondPic = 1
+                }
+                2 -> {
+                    pic2.setImageResource(R.drawable.cherries)
+                    secondPic = 2
+                }
+                3 -> {
+                    pic2.setImageResource(R.drawable.lemon)
+                    secondPic = 3
+                }
+                4 -> {
+                    pic2.setImageResource(R.drawable.game7)
+                    secondPic = 4
+                }
+            }
+            when (generateRandomNumber()) {
+                1 -> {
+                    pic3.setImageResource(R.drawable.bell)
+                    thirdPic = 1
+                }
+                2 -> {
+                    pic3.setImageResource(R.drawable.cherries)
+                    thirdPic = 2
+                }
+                3 -> {
+                    pic3.setImageResource(R.drawable.lemon)
+                    thirdPic = 3
+                }
+                4 -> {
+                    pic3.setImageResource(R.drawable.game7)
+                    thirdPic = 4
+                }
+            }
+
+            if (firstPic == secondPic && secondPic == thirdPic && firstPic == thirdPic) {
+                lblMessage.text = "Congrats! You have matched all three pictures and gained 25 coins!"
+                counter += 25
+                //lblCoinCounter.text = "Coins = " + counter
+            }
+            else if (firstPic == secondPic || secondPic == thirdPic || firstPic == thirdPic){
+                lblMessage.text = "Congrats! You have matched two pictures and gained 5 coins!"
+                //counter += 5
+                //lblCoinCounter.text = "Coins = " + counter
+            }
+            else {
+                lblMessage.text = "Sorry, you did not match any pictures. Lose 10 coins..."
+                //counter -= 10
+                //lblCoinCounter.text = "Coins = " + counter
+            }
+        }
     }
 }
--- app/src/main/java/com/example/administrator/lotterygame/MainActivity.kt
+++ app/src/main/java/com/example/administrator/lotterygame/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.administrator.lotterygame
 
-import android.media.Image
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.ImageView
+import android.support.v4.content.ContextCompat.startActivity
+import com.example.administrator.lotterygame.R.id.btnSubmit
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-import android.graphics.drawable.BitmapDrawable
-import android.graphics.Bitmap
-
-
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,72 +14,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        spin()
-
+        setOnClickListenerForButton()
     }
 
-    //Generates a random number between 1 and 4. Will be used to display pictures later
-    private fun generateRandomNumber(): Int {
-        val number = Random()
-        val min = 1
-        val max = 4
-        val num = number.nextInt(max + 1 - min) + min
-        return num
-    }
-
-    private fun spin() {
-        var counter = 0
-
-        btnSpin.setOnClickListener() {
-            when (generateRandomNumber()) {
-                1 -> pic1.setImageResource(R.drawable.bell)
-                2 -> pic1.setImageResource(R.drawable.cherries)
-                3 -> pic1.setImageResource(R.drawable.lemon)
-                4 -> pic1.setImageResource(R.drawable.game7)
-            }
-            when (generateRandomNumber()) {
-                1 -> pic2.setImageResource(R.drawable.bell)
-                2 -> pic2.setImageResource(R.drawable.cherries)
-                3 -> pic2.setImageResource(R.drawable.lemon)
-                4 -> pic2.setImageResource(R.drawable.game7)
-            }
-            when (generateRandomNumber()) {
-                1 -> pic3.setImageResource(R.drawable.bell)
-                2 -> pic3.setImageResource(R.drawable.cherries)
-                3 -> pic3.setImageResource(R.drawable.lemon)
-                4 -> pic3.setImageResource(R.drawable.game7)
-            }
-
-            /*Log.e("Pic2 is showing", pic2.getDrawable().toString())
-            Log.e("Pic1 is showing", pic1.getDrawable().toString())
-            Log.e("Pic3 is showing", pic3.getDrawable().toString())
-            Log.e("Is pic2 = pic1", pic1.getDrawable().toString().equals(pic2.getDrawable().toString()).toString())
-
-            val bitmap = (pic1.getDrawable() as BitmapDrawable).bitmap
-            val bitmap2 = (pic2.getDrawable() as BitmapDrawable).bitmap
-
-            if (bitmap == bitmap2) {
-                Log.e("Pic2 is showing", pic2.getDrawable().toString())
-            }*/
-
-            if (pic1.getDrawable().toString() == pic2.getDrawable().toString() &&
-                    pic2.getDrawable().toString() == pic3.getDrawable().toString() &&
-                    pic1.getDrawable().toString() == pic3.getDrawable().toString()) {
-
-                lblMessage.text = ("Congrats! You have matched all three numbers and gained 20 coins!")
-                counter += 20
-                lblCoinCounter.text = ("Coins = " + counter)
-            } else if (pic1.getDrawable().toString() == pic2.getDrawable().toString() ||
-                    pic2.getDrawable().toString() == pic3.getDrawable().toString() ||
-                    pic1.getDrawable().toString() == pic3.getDrawable().toString()) {
-                lblMessage.text = ("Congrats! You have matched two numbers and gained 10 coins!")
-                counter += 10
-                lblCoinCounter.text = ("Coins = " + counter)
-            } else {
-                lblMessage.text = ("You did not match any numbers. Lose 15 coins!")
-                counter -= 10
-                lblCoinCounter.text = ("Coins = " + counter)
-            }
+    private fun setOnClickListenerForButton() {
+        btnSubmit.setOnClickListener {
+            val intent = Intent("com.example.administrator.lotterygame.Main2Activity")
+            startActivity(intent)
         }
     }
 }
